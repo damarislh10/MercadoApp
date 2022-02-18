9a7de8581d12130548a7a9b1284bcbff6aad9d71
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-02-18 09:30:04 -0500

    changes dependencias

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Container, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import "../styles/navBar.css";
 const NavBar = () => {
